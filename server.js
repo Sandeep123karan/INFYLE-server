819c3ba7fd2add2c1d33ef91f6052c7b1ab86dd1
Sandeep123karan <sandeepkaran234@gmail.com>
2025-12-26 14:52:06 +0530

    fix vercel serverless setup

--- server.js
+++ server.js
@@ -12,12 +12,23 @@ dotenv.config();
 const app = express();
 
 // Enable CORS for frontend
+// app.use(cors({
+//   origin: ["http://localhost:3000","http://localhost:5173","http://localhost:5174","http://localhost:5175"], // React frontend
+//   methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+//   allowedHeaders: ["Content-Type","Authorization"],
+//   credentials: true
+// }));
 app.use(cors({
-  origin: ["http://localhost:3000","http://localhost:5173","http://localhost:5174","http://localhost:5175"], // React frontend
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://client-main-two.vercel.app"
+  ],
   methods: ["GET","POST","PUT","DELETE","OPTIONS"],
   allowedHeaders: ["Content-Type","Authorization"],
   credentials: true
 }));
+
 
 // Handle preflight requests
 app.options("*", cors());
@@ -56,14 +67,23 @@ app.use("/api/vendor/products", require("./routes/vendorProductRoutes"));
 app.use("/api/vendor/subcategories", require("./routes/vendorSubCategoryRoutes"));
 
 // Connect to MongoDB & start server
-const PORT = process.env.PORT || 7002;
+// const PORT = process.env.PORT || 7002;
+
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     console.log("✅ MongoDB Connected");
+//     app.listen(PORT, () =>
+//       console.log(`🚀 Server running on port ${PORT}`)
+//     );
+//   })
+//   .catch((err) => console.log("❌ MongoDB Error:", err.message));
+
+
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("✅ MongoDB Connected");
-    app.listen(PORT, () =>
-      console.log(`🚀 Server running on port ${PORT}`)
-    );
-  })
+  .then(() => console.log("✅ MongoDB Connected"))
   .catch((err) => console.log("❌ MongoDB Error:", err.message));
+
+module.exports = app;
